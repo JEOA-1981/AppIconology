c6d02860b92ae0bba06af4a901cc5a0470984b12
JEOA-1981 <jeduardo.oliv@gmail.com>
2021-04-09 18:18:09 -0500

    Update AppIconology.py

--- src/AppIconology.py
+++ src/AppIconology.py
@@ -58,65 +58,50 @@ def bienvenida():
     st.markdown('https://www.linkedin.com/in/jes%C3%BAs-eduardo-oliva-abarca-78b615157/')
 
 @st.cache
-def process_image(image):
-    blob = cv2.dnn.blobFromImage(
-        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
-    )
-    net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODELO)
-    net.setInput(blob)
-    detections = net.forward()
-    return detections
+def procesamiento(imagen):
+    blob = cv2.dnn.blobFromImage(cv2.resize(imagen, (300, 300)), 0.007843, (300, 300), 127.5)
+    red = cv2.dnn.readNetFromCaffe(PROTOTXT, MODELO)
+    red.setInput(blob)
+    detecciones = red.forward()
+    return detecciones
 
 @st.cache
-def annotate_image(
-    image, detections, confidence_threshold=UMBRAL_CONFIANZA_DEFECTO
-):
-    # loop over the detections
-    (h, w) = image.shape[:2]
-    labels = []
-    for i in np.arange(0, detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
+def anotar(imagen, detecciones, umbral_confianza= UMBRAL_CONFIANZA_DEFECTO):
+    (h, w) = imagen.shape[:2]
+    etiquetas = []
+    for i in np.arange(0, detecciones.shape[2]):
+        confianza = detecciones[0, 0, i, 2]
 
-        if confidence > confidence_threshold:
-            # extract the index of the class label from the `detections`,
-            # then compute the (x, y)-coordinates of the bounding box for
-            # the object
-            idx = int(detections[0, 0, i, 1])
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
-
-            # display the prediction
-            label = f"{CLASES[idx]}: {round(confidence * 100, 2)}%"
-            labels.append(label)
-            cv2.rectangle(image, (startX, startY), (endX, endY), COLORES[idx], 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(
-                image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORES[idx], 2
+        if confianza > umbral_confianza:
+            idx = int(detecciones[0, 0, i, 1])
+            caja = detecciones[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (inicioX, inicioY, finX, finY) = caja.astype("int")
+            etiqueta = f"{CLASES[idx]}: {round(confianza * 100, 2)}%"
+            etiquetas.append(etiqueta)
+            cv2.rectangle(imagen, (inicioX, inicioY), (finX, finY), COLORES[idx], 2)
+            y = inicioY - 15 if inicioY - 15 > 15 else inicioY + 15
+            cv2.putText(imagen, etiqueta, (inicioX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORES[idx], 2
             )
-    return image, labels
+    return imagen, etiquetas
 
 def identificacion_objetos():
    st.title('Detección de objetos con MobileNet SSD')
-   img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-   confidence_threshold = st.slider(
-       "Confidence threshold", 0.0, 1.0, UMBRAL_CONFIANZA_DEFECTO, 0.05
-   )
+   archivo = st.file_uploader('Sube una imagen (se admiten archivos .png, .jpg y .jpeg)', type=["png", "jpg", "jpeg"])
+   umbral_confianza = st.slider('Umbral de confianza', 0.0, 1.0, UMBRAL_CONFIANZA_DEFECTO, 0.05)
 
-   if img_file_buffer is not None:
-       image = np.array(Image.open(img_file_buffer))
+   if archivo is not None:
+       imagen = np.array(Image.open(archivo))
 
    else:
-       demo_image = IMAGEN_DEMO
-       image = np.array(Image.open(demo_image))
+       imagen_demo = IMAGEN_DEMO
+       imagen = np.array(Image.open(imagen_demo))
 
-   detections = process_image(image)
-   image, labels = annotate_image(image, detections, confidence_threshold)
+   detecciones = procesamiento(imagen)
+   imagen, etiquetas = anotar(imagen, detecciones, umbral_confianza)
 
-   st.image(
-       image, caption=f"Processed image", use_column_width=True,
-   )
+   st.image(imagen, caption= 'Imagen procesada', use_column_width= True,)
 
-   st.write(labels)
+   st.write(etiquetas)
 
 def load_image(filename):
     image = cv2.imread(filename)
