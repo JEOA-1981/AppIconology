05a2cb934aebadba0b5161646f5805cd637cc684
JEOA-1981 <jeduardo.oliv@gmail.com>
2021-04-09 13:47:27 -0500

    Update AppIconology.py

--- src/AppIconology.py
+++ src/AppIconology.py
@@ -98,7 +98,7 @@ def photo():
         original = Image.open('images/imagen01.jpg')
         st.image(original, use_column_width=True)
         
-    image = cv2.imread('images/imagen01.jpg')
+    image = cv2.imread(r'images/imagen01.jpg')
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     x = st.slider('Change Threshold value',min_value = 50,max_value = 255)
