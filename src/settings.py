c6d680e0d4eca788ac3b335a0df07f48570e7caf
JEOA-1981 <jeduardo.oliv@gmail.com>
2021-04-09 20:18:33 -0500

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,4 @@
 UMBRAL_CONFIANZA_DEFECTO = 0.5
-IMAGEN_DEMO = "images/demo.jpg"
-MODELO = "model/MobileNetSSD_deploy.caffemodel"
-PROTOTXT = "model/MobileNetSSD_deploy.prototxt.txt"
+IMAGEN_DEFECTO = 'images/carrington_autorretrato.jpg'
+MODELO = 'model/MobileNetSSD_deploy.caffemodel'
+PROTOTXT = 'model/MobileNetSSD_deploy.prototxt.txt'
