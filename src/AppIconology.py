ab133d9c178c7b9821e45fbb7d5b5f43b7adaac7
JEOA-1981 <jeduardo.oliv@gmail.com>
2021-08-18 22:53:02 -0500

    Update AppIconology.py

--- src/AppIconology.py
+++ src/AppIconology.py
@@ -136,7 +136,7 @@ def deteccion_contornos_bordes():
     
     boton_uno, boton_dos = st.beta_columns(2)
     with boton_uno:
-        if st.checkbox('Detectar bordes', key= 0):
+        if st.checkbox('Detectar bordes', key= 4):
             bordes = cv2.Canny(imagen,100,200)
             st.image([imagen, bordes])
     
